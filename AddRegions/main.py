5c79b2264ce1d17e55d9193b3c72d0db7647d68d
Aksel <ahsorbye@gmail.com>
2021-02-09 15:44:34 +0100

    Add files for adding new regions

---
+++ AddRegions/main.py
@@ -0,0 +1,166 @@
+# from xlrd import open_workbook
+# book = open_workbook('Inputdata\\Data_Europe_openENTRANCE_GradualDevelopment_oE_v05_kh_30_12_2020.xlsx')
+import pandas as pd
+import numpy as np
+
+try:
+    open('Output.txt', 'w').close()
+except:
+    pass
+
+new_regions = ["NO" + str(i) for i in range(1, 6)]
+n = len(new_regions)
+datafile = 'Data_Europe_openENTRANCE_GradualDevelopment_oE_v05_kh_30_12_2020.xlsx'
+skip_rows = 4
+
+datafile = 'Hourly_Data_Europe_v05_kh_29_12_2020.xlsx'
+skip_rows = 0
+
+def fprint(*args, **kwargs):
+    print(*args, **kwargs)
+    with open('Output.txt', 'a') as file:
+        print(*args, **kwargs, file=file)
+
+
+def isNaN(val):
+    return val != val
+
+
+def colnum_string(n):
+    string = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        string = chr(65 + remainder) + string
+    return string
+
+def getIndexes(df, value):
+    listOfPos = []
+
+    # isin() method will return a dataframe with
+    # boolean values, True at the positionsag
+    # where element exists
+    result = df.isin([value])
+
+    # any() method will return
+    # a boolean series
+    seriesObj = result.any()
+
+    # Get list of column names where
+    # element exists
+    columnNames = list(seriesObj[seriesObj == True].index)
+
+    # Iterate over the list of columns and
+    # extract the row index where element exists
+    for col in columnNames:
+        rows = list(result[col][result[col] == True].index)
+
+        for row in rows:
+            # idx=df.columns.get_loc(col)
+            listOfPos.append((row, col))
+
+    return listOfPos
+
+
+def printPositions(listOfPositions):
+    if listOfPositions != []:
+        fprint(name, ":", sep="")
+        for location in listOfPositions:
+            fprint(location)
+            """row=location[0]+2+3
+            letter=colnum_string(location[1]+1)
+            pretty_loc=(row,letter)
+            fprint(pretty_loc)"""
+
+
+def addRows(df, listOfPositions):
+    delete = []
+    for pos in listOfPositions:
+        row_nr = pos[0]
+        delete.append(row_nr)
+        row = (df.iloc[row_nr, :])
+        rep = new_regions.copy()
+        rep.insert(0, "NO")
+        for i in range(n):
+            row = row.replace(rep[i], rep[i + 1])
+            df = df.append(row)
+
+    df = df.reset_index(drop=True)
+    df = df.drop(df.index[delete])
+    return df
+
+
+def addCols(df):
+    series = df["NO"]
+    del df["NO"]
+    for reg in new_regions:
+        df[reg] = series
+
+    return df
+
+
+def toExcel(dfs, names, filename):
+    writer = pd.ExcelWriter('ModifiedInput\\Modified' + filename)
+
+    for df, n in zip(dfs, names):
+        df = df.dropna(axis=0, how='all')
+        df = df.dropna(axis=1, how='all')
+        df=df.T.reset_index().T
+        for idx,col in enumerate(df):
+            val=df.iloc[0,idx]
+            if type(val)==str and 'Unnamed' in val:
+                df.iloc[0,idx] = ''
+
+        df.to_excel(writer, sheet_name=n, header=False, index=False)
+
+
+    writer.save()
+print("Reading data")
+cut_sheets = pd.read_excel('Inputdata\\' + datafile, sheet_name=None, skiprows=range(0, skip_rows))
+original_sheets = pd.read_excel('Inputdata\\' + datafile, sheet_name=None)
+dfs = []
+names = []
+print("Adding regions")
+for name, sheet in cut_sheets.items():
+    if name == "Sets":
+        continue
+
+    #sheet = sheet.dropna(thresh=len(sheet) // nanTresh, axis=1)
+    add = False
+    listOfPositions = getIndexes(sheet, "NO")
+    if listOfPositions != []:
+        # printPositions(listOfPositions)
+        sheet = addRows(sheet, listOfPositions)
+        # print(sheet)
+        add = True
+
+    if "NO" in sheet.columns:
+        sheet = addCols(sheet)
+        add = True
+
+    if add:
+        dfs.append(sheet)
+        names.append(name)
+
+for name, sheet in original_sheets.items():
+    if name == "Sets":
+        regions = list(sheet["Region"])
+        regions = [i for i in regions if i != "NO"]
+        c = 0
+        for idx, reg in enumerate(regions):
+            if isNaN(reg):
+                regions[idx] = new_regions[c]
+                c += 1
+
+            if c == len(new_regions):
+                break
+
+        series = pd.Series(regions)
+        sheet["Region"] = series
+
+    if name not in names:
+        #sheet = sheet.dropna(thresh=len(sheet) // nanTresh, axis=1)
+        dfs.append(sheet)
+        names.append(name)
+
+print("Writing to Excel")
+toExcel(dfs, names, datafile)
