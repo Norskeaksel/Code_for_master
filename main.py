21e374e667720bc1b303683bb363ef2a6e4a814a
Aksel <ahsorbye@gmail.com>
2021-04-27 13:04:16 +0200

    Show bar numbers

--- main.py
+++ main.py
@@ -12,31 +12,43 @@ from plotingFunctions import *
 # from IPython.core.interactiveshell import InteractiveShell
 # InteractiveShell.ast_node_interactivity = "all"
 
-Alltech = "Alltech\\"
+Alltech = "Alltech\\"#Alltech\\" #"HydroWindThermalPV\\" "Alltech\\"
 # Read our new capacities
-SCC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"SC\\totalPowerCapacitiesNO.csv")
-TFC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"TF\\totalPowerCapacitiesNO.csv")
-DTC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"DT\\totalPowerCapacitiesNO.csv")
+#SCC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"SC\\totalPowerCapacitiesNO.csv")
+#TFC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"TF\\totalPowerCapacitiesNO.csv")
+#DTC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"DT\\totalPowerCapacitiesNO.csv")
 GDC = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacitiesNO.csv")
+#GDC48 = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacities48.csv")
+#CDCTrade= pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacitiesTrade.csv")
+CDCelectric= pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacitiesElectric.csv")
+CDCelectricTrade=pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacitiesElectricTrade.csv")
+#GDCDynamicCF = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacitiesDynamicCF.csv")
 
 # Read our new productions
-SCP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"SC\\totalPowerProductionsNO.csv")
-TFP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"TF\\totalPowerProductionsNO.csv")
-DTP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"DT\\totalPowerProductionsNO.csv")
+#SCP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"SC\\totalPowerProductionsNO.csv")
+#TFP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"TF\\totalPowerProductionsNO.csv")
+#DTP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"DT\\totalPowerProductionsNO.csv")
+#GDPTrade = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductionsTrade.csv")
 GDP = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductionsNO.csv")
+GDPelectric = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductionsElectric.csv")
+GDPelectricTrade = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductionsElectricTrade.csv")
+GDPtradeAndPower = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductionsTradeAndPower.csv")
+#GDP48 = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductions48.csv")
+#GDPDynamicCF = pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerProductionsDynamicCF.csv")
+#NVEP=pd.read_csv("Rdomain\\Tables\\"+Alltech+"NVE\\totalPowerProductions.csv")
 
 # # Read TU Capacities
-TU_SCC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"SC\\totalPowerCapacitiesNO.csv")
-TU_TFC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"TF\\totalPowerCapacitiesNO.csv")
-TU_DTC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"DT\\totalPowerCapacitiesNO.csv")
-TU_GDC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"GD\\totalPowerCapacitiesNO.csv")
+#TU_SCC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"SC\\totalPowerCapacitiesNO.csv")
+#TU_TFC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"TF\\totalPowerCapacitiesNO.csv")
+#TU_DTC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"DT\\totalPowerCapacitiesNO.csv")
+#TU_GDC = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"GD\\totalPowerCapacitiesNO.csv")
 # TU_GDC_Old = pd.read_csv("Rdomain\\Tables\\"+Alltech+"TUresults\\GD\\Old\\totalPowerCapacities.csv")
 #
 # # Read TU productions
-TU_TFP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"TF\\totalPowerProductionsNO.csv")
-TU_SCP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"SC\\totalPowerProductionsNO.csv")
-TU_DTP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"DT\\totalPowerProductionsNO.csv")
-TU_GDP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"GD\\totalPowerProductionsNO.csv")
+#TU_TFP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"TF\\totalPowerProductionsNO.csv")
+#TU_SCP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"SC\\totalPowerProductionsNO.csv")
+#TU_DTP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"DT\\totalPowerProductionsNO.csv")
+#TU_GDP = pd.read_csv("Rdomain\\Tables\\TUresults\\"+Alltech+"GD\\totalPowerProductionsNO.csv")
 #TU_GDP_Old = pd.read_csv("Rdomain\\Tables\\"+Alltech+"TUresults\\GD\\Old\\totalPowerProductions.csv")
 #
 # # Read TF regions
@@ -71,8 +83,8 @@ TF_NO2P = pd.read_csv("Rdomain\\Tables\\TF\\totalPowerProductionsNO2.csv")
 # Si_SCP = pd.read_csv("Rdomain\\Tables\\" + Alltech + "Signy\\SC\\totalPowerProductionsNO.csv")
 
 # Read MiddleEarth files
-MC=[pd.read_csv("Rdomain\\Tables\\"+Alltech+"MiddleEarth\\totalPowerCapacitiesMordor"+str(i)+".csv") for i in range(1,6)]
-MP=[pd.read_csv("Rdomain\\Tables\\"+Alltech+"MiddleEarth\\totalPowerProductionsMordor"+str(i)+".csv") for i in range(1,6)]
+#MC=[pd.read_csv("Rdomain\\Tables\\"+Alltech+"MiddleEarth\\totalPowerCapacitiesMordor"+str(i)+".csv") for i in range(1,6)]
+#MP=[pd.read_csv("Rdomain\\Tables\\"+Alltech+"MiddleEarth\\totalPowerProductionsMordor"+str(i)+".csv") for i in range(1,6)]
 
 # Read GD files
 # NO_GDC = [pd.read_csv("Rdomain\\Tables\\"+Alltech+"GD\\totalPowerCapacities.csv")]#+str(i)+".csv") for i in range(1,6)]
@@ -85,23 +97,30 @@ i=""
 #            ]for i in range(1, 6)]
 
 # read technologyUses
-i=""
+'''i=""
 NO_SC_use = [pd.read_csv("Rdomain\\Tables\\" + Alltech + "SC\\technologyUsesNO" + str(i) + ".csv") for _ in range(1,2)]
 numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 df=NO_SC_use[0]
 for c in [c for c in df.columns if df[c].dtype in numerics]:
     df[c] = df[c].abs()
-NO_SC_use=df
+NO_SC_use=df'''
 # plotDfs(NO_DT_PB, "Power Balance [Twh] Directed Transition",False)
 
 #plotDfs([TF_NO1C,TF_NO2C], "TF region Capacities", False)
 #plotDfs([DTC, TU_DTC], "Power Capacities [GW] Directed Transition Gams 34 vs TU" ,False)
 #plotDfs([DTP, TU_DTP], "Power Productions [TWh] Directed Transition Gams 34 vs TU",False)
-#plotDfs([GDC, TU_GDC], "GradualDevelopment Power Capacities [GW] Gams 34 vs TU", False)
-#plotDfs([GDP, TU_GDP], "GradualDevelopment Power Productions [TWh] Gams 34 vs TU", False)
+#plotDfs([GDC, CDCelectricTrade, ], "GradualDevelopment Power Capacities [GW] Original vs Updates vs NVE", False)
+unit='TWh'
+ascending=False
+NVE=False
+showNumbers=True
+#plotDfs([GDPDynamicCF, GDPelectric, GDPelectricTrade, NVEP], "GradualDevelopment Power Productions ["+unit+"] Dynamic vs Electric vs Trade vs NVE", unit, ascending, NVE, showNumbers)
+plotDfs([GDP, GDPelectricTrade, GDPtradeAndPower], "GradualDevelopment Power Productions ["+unit+"] Electric Trade vs Trade and power", unit, ascending, NVE, showNumbers)
+
+#plotDfs([GDP,GDPDynamicCF,NVEP],"Original vs Dynamic vs NVE's Power Productions [TWh]",False,True)
 #plotDfs([TFC, TU_TFC], "TechnoFriendly Power Capacities [GW] Gams 34 vs TU", False)
 #plotDfs([TFP, TU_TFP], "TechnoFriendly Power Productions [TWh] Gams 34 vs TU", False)
-plotDfs([SCC, TU_SCC], "SocietalCommitment Power Capacities [GW] GG vs RR", False)
+#plotDfs([SCC, TU_SCC], "SocietalCommitment Power Capacities [GW] GG vs RR", False)
 #plotDfs([SCP, TU_SCP], "SocietalCommitment Power Productions [TWh] GG vs RR", False)
 #plotDfs([NO_SC_use], "SocietalCommitment Technology Power Use", False)
 
--- plotingFunctions.py
+++ plotingFunctions.py
@@ -1,10 +1,25 @@
 # %%
 import random
+from collections import defaultdict
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
+
 # plt.matplotlib.use('Qt5Agg') #Show plot in separate window
+
+def makeStr(val):
+    if val == 0:
+        string = ""
+    else:
+        string = str(val)
+        if len(string) > 3:
+            string = string[0:3]
+        if string[-1] == '.':
+            string = string[0:-1]
+    return string
+
 
 # %%
 colorMap = {"p": "#7e1e9c",
@@ -20,19 +35,20 @@ colorMap = {"p": "#7e1e9c",
             "bb": "#0165fc",
             "wb": "#a2cffe",
             "yg": "#c0fb2d",
-            "gold":"#dbb40c",
+            "gold": "#dbb40c",
             "sfg": "#7af9ab",
             "bg": "#137e6d",
             "fg": "#06470c",
             "ochre": "#bf9005",
             "kg": "#02ab2e",
             "lo": "#fdaa48"
-}
+            }
 techColor = {
     "Hydro": "b",
     "Thermal": "r",
     "Other": "r",
     "Wind Onshore": "g",
+    "Wind": "g",
     "PV": "o",
     "Wind Offshore Transitional": "c",
     "Wind Offshore Deep": "p",
@@ -40,9 +56,9 @@ techColor = {
     "Import": "c",
     "Production": "b",
     "Use": "r",
-    
+
     "P_Biomass": "bur",
-    "HLI_Biomass_CHP_CCS" : "r",
+    "HLI_Biomass_CHP_CCS": "r",
     "P_Gas": "hp",
     "RES_Hydro_Large": "bb",
     "RES_Hydro_Small": "wb",
@@ -57,46 +73,76 @@ techColor = {
     "RES_Wind_Onshore_Opt": "fg"
 }
 
-# %%
 
-def plotDfs(dfs, title, ascending=True):
+# %%
+def plotDfs(dfs, title, unit, ascending=True, NVE=False,showNumbers=False):
     years = list(map(str, range(2015, 2051, 5)))
+    if NVE:
+        kill = ['2045', '2050']
+        for i in kill:
+            years.remove(i)
+
     x = np.arange(len(years))
     width = 1.55 - 0.15 * len(dfs)
     opacity = 1
     fig, ax = plt.subplots()
     plt.grid(axis="y", zorder=0)
-    #plt.ylim([0, 50])
+    # plt.ylim([0, 50])
+    nonZeroTechnologies=set()
     for i in range(len(dfs)):
+        prev = defaultdict(int)
         df = dfs[i]
-        df = df[df.Technology != "Sum"]
-        #df = df.sort_values(by='2015', ascending=ascending)
+        # df = df[df.Technology != "Sum"]
+        # df = df.sort_values(by='2015', ascending=ascending)
         base = 0
         negative_base = 0
         for trueIdx, (idx, row) in enumerate(df.iterrows()):
             use_base = True
             tech = row['Technology']
-            #if sum(np.array(row[2::])) == 0:
-                #continue
-            if row[years][-1]<0:
+            # if sum(np.array(row[2::])) == 0:
+            # continue
+            if row[years].sum():
+                nonZeroTechnologies.add(tech)
+
+            if row[years][-1] < 0:
                 use_base = False
             try:
                 color = colorMap[techColor[tech]]
             except:
-                techColor[tech]=tech
-                colorMap[tech]=tuple(random.choice(range(32, 256, 32)) / 255 for i in range(4))
+                techColor[tech] = tech
+                colorMap[tech] = tuple(random.choice(range(32, 256, 32)) / 255 for i in range(4))
                 color = colorMap[tech]
 
-            ax.bar(np.array(list(map(int, years))) + (i - (len(dfs) - 1) / 2) * width * 1.1, row[years], width,
-                   label=tech,
-                   bottom=base if use_base else negative_base, alpha=opacity, zorder=3, color=color)
+            barsXPossitions = np.array(list(map(int, years))) + (i - (len(dfs) - 1) / 2) * width * 1.1
+            if tech != "Sum":
+                ax.bar(barsXPossitions, row[years], width,
+                       label=tech,
+                       bottom=base if use_base else negative_base, alpha=opacity, zorder=3, color=color)
+                ax.set_xlabel("Year", fontsize=14)
+                ax.set_ylabel(unit, fontsize=14)
+
             if use_base:
                 base += row[years]
             else:
                 negative_base += row[years]
 
-    patches=[mpatches.Patch(color=colorMap[techColor[tech]], label=tech) for tech in df['Technology']]
-    ax.legend(handles=patches)
+            if showNumbers:
+                for idx, xPos in enumerate(barsXPossitions):
+                    year = years[idx]
+                    val = row[year]
+                    yPos = prev[year] + val / 2
+                    color = "white"
+                    if tech=="Sum":
+                        yPos=prev[year]+5
+                        color = "black"
+
+                    #string=makeStr(val)
+                    string = "%d" % val if round(val) else ""
+                    ax.text(xPos, yPos, string, ha="center", va="center", color=color, fontsize=9, fontweight="bold")
+                    prev[year] += row[year]
+
+    patches = [mpatches.Patch(color=colorMap[techColor[tech]], label=tech) for tech in df['Technology'] if tech!="Sum" and tech in nonZeroTechnologies]
+    ax.legend(handles=patches, loc="upper left")
     plt.title(title)
     # manager = plt.get_current_fig_manager()
     # manager.window.showMaximized()
@@ -105,5 +151,3 @@ def plotDfs(dfs, title, ascending=True):
     figure.set_size_inches(14, 8)  # set figure's size manually to your full screen (32x18)
     plt.savefig("plots\\" + title)  # , bbox_inches='tight')  # bbox_inches removes extra white spaces
     plt.show()
-
-
